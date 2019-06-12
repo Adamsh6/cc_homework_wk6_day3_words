e83a60374cf89498c669ecce09be4e7ca5530899
Adamsh6 <adam.s.houten@gmail.com>
2019-06-12 14:52:03 +0100

    completed isogram_finder

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,9 +1,23 @@
 const IsogramFinder = function (word) {
+  this.wordArray = word.toLowerCase().split('')
 
 }
 
-IsogramFinder.prototype.isIsogram = function () {
+IsogramFinder.prototype.isARepeatedLetter = function(primeIndex) {
+  const primeLetter = this.wordArray[primeIndex]
+  slicedWord = this.wordArray
+  slicedWord.splice(primeIndex, 1)
+  result = slicedWord.includes(primeLetter)
+  return result
+}
 
+IsogramFinder.prototype.isIsogram = function () {
+  result = this.wordArray.every((letter, index) => {
+    // console.log(!(this.isARepeatedLetter(index)));
+    return !(this.isARepeatedLetter(index))
+
+  })
+  return result
 }
 
 module.exports = IsogramFinder;
