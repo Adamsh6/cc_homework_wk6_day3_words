e17cffb3f14c174d424097c3e208ffcdb069f7ae
Adamsh6 <adam.s.houten@gmail.com>
2019-06-12 14:18:01 +0100

    completed pangram_finder

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,9 +1,13 @@
 const PangramFinder = function (phrase) {
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+  this.phrase = phrase;
 }
 
 PangramFinder.prototype.isPangram = function () {
-
+  const result = this.alphabet.every((letter) => {
+    return this.phrase.toLowerCase().includes(letter)
+  })
+  return result
 }
 
 module.exports = PangramFinder;
