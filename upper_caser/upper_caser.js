3148ca8390c1efe6f8f788c7ca84c790d6f5dd1f
Adamsh6 <adam.s.houten@gmail.com>
2019-06-12 14:09:48 +0100

    finished upper_caser

--- upper_caser/upper_caser.js
+++ upper_caser/upper_caser.js
@@ -1,8 +1,12 @@
 const UpperCaser = function (words) {
+  this.words = words
 }
 
 UpperCaser.prototype.toUpperCase = function () {
-
+  const upWords = this.words.map((word) => {
+    return word.toUpperCase();
+  })
+  return upWords
 }
 
 module.exports = UpperCaser;
